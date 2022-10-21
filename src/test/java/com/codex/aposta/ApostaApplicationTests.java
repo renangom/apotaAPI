ff3ca1cd26fa4283c3e6c41d8376aed23478f473
Renan <renan23eduardo@gmail.com>
2022-10-21 09:50:20 -0300

    Create maven java project

---
+++ src/test/java/com/codex/aposta/ApostaApplicationTests.java
@@ -0,0 +1,19 @@
+package com.codex.aposta;
+
+import java.time.LocalDate;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.codex.aposta.model.Pessoa;
+
+@SpringBootTest
+class ApostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
